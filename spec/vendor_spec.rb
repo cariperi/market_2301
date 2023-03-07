5b0f7dbbbffcc98069e1bc5f2f08b041da1e0d72
Caroline Peri <caroline.anne.peri@gmail.com>
2023-03-07 11:12:28 -0500

    test: add initialize tests for vendor

--- spec/vendor_spec.rb
+++ spec/vendor_spec.rb
@@ -1,1 +1,24 @@
 require 'spec_helper'
+
+describe Vendor do
+  before(:each) do
+    @vendor = Vendor.new("Rocky Mountain Fresh")
+    @item1 = Item.new({name: 'Peach', price: "$0.75"})
+    @item2 = Item.new({name: 'Tomato', price: "$0.50"})
+  end
+
+  describe '#initialize' do
+    it 'exists' do
+      expect(@vendor).to be_a Vendor
+    end
+
+    it 'has a name' do
+      expect(@vendor.name).to eq("Rocky Mountain Fresh")
+    end
+
+    it 'has inventory that starts empty by default' do
+      expect(@vendor.inventory).to eq({})
+      expect(@vendor.inventory).to be_a Hash
+    end
+  end
+end
