1c427228dd84c64bcbb0d567fe8713fd45a9ad87
Caroline Peri <caroline.anne.peri@gmail.com>
2023-03-07 12:02:04 -0500

    feat: add sorted item list to market class

--- lib/market.rb
+++ lib/market.rb
@@ -19,4 +19,14 @@ class Market
     #@vendors.select{|vendor| vendor.inventory.keys.include?(item)}
     @vendors.select{|vendor| vendor.check_stock(item) != 0}
   end
+
+  def sorted_item_list
+    items = []
+    @vendors.each do |vendor|
+      vendor.inventory.each do |item, _|
+        items << item.name if !items.include?(item.name)
+      end
+    end
+    items.sort
+  end
 end
