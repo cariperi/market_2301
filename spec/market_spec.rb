1cac13157d076d672d4a2713978fc8180796eb4f
Caroline Peri <caroline.anne.peri@gmail.com>
2023-03-07 11:30:29 -0500

    test: add setup and initialize tests for market spec

--- spec/market_spec.rb
+++ spec/market_spec.rb
@@ -1,1 +1,29 @@
 require 'spec_helper'
+
+describe Market do
+  before(:each) do
+    @market = Market.new("South Pearl Street Farmers Market")
+    @vendor1 = Vendor.new("Rocky Mountain Fresh")
+    @vendor2 = Vendor.new("Ba-Nom-a-Nom")
+    @vendor3 = Vendor.new("Palisade Peach Shack")
+    @item1 = Item.new({name: 'Peach', price: "$0.75"})
+    @item2 = Item.new({name: 'Tomato', price: "$0.50"})
+    @item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
+    @item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
+  end
+
+  describe '#initialize' do
+    it 'exists' do
+      expect(@market).to be_a Market
+    end
+
+    it 'has a name' do
+      expect(@market.name).to eq("South Pearl Street Farmers Market")
+    end
+
+    it 'has vendors that start as empty by default' do
+      expect(@market.vendors).to eq([])
+      expect(@market.vendors).to be_a Array
+    end
+  end
+end
