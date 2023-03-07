2bc7ff737bc7b9c41c02ce90fb46d25d204ab669
Caroline Peri <caroline.anne.peri@gmail.com>
2023-03-07 11:23:43 -0500

    feat: add stock method and update check stock method and inventory hash at init

--- lib/vendor.rb
+++ lib/vendor.rb
@@ -4,10 +4,15 @@ class Vendor
 
   def initialize(name)
     @name = name
-    @inventory = {}
+    @inventory = Hash.new(0)
   end
 
   def check_stock(item)
     return 0 if !@inventory.keys.include?(item)
+    @inventory[item]
+  end
+
+  def stock(item, amount)
+    @inventory[item] += amount
   end
 end
